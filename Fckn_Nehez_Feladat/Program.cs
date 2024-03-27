c1ac6c3c818998d43ffaffa8101ef9bab9a42843
unknown <headt@stud.uni-obuda.hu>
2024-03-27 12:42:49 +0100

    Empty Project

---
+++ Fckn_Nehez_Feladat/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Fckn_Nehez_Feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia!!!");
+
+        }
+    }
+}
